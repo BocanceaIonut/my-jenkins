41b1da9ab9e8d5c7c01ba189be9f887f9d4d2a00
i.bocancea <i.bocancea@klarna.com>
2025-03-30 15:30:28 +0200

    try 5

--- jobs/jobdsl/services/pipeline.groovy
+++ jobs/jobdsl/services/pipeline.groovy
@@ -1,6 +1,52 @@
 import service.MyPipeline
 import javaposse.jobdsl.dsl.DslFactory
 
+package service
+
+import javaposse.jobdsl.dsl.DslFactory
+
+class MyPipeline {
+    private DslFactory dslFactory
+    
+    MyPipeline(DslFactory dslFactory) {
+        this.dslFactory = dslFactory
+    }
+    
+    def build = dslFactory.pipelineJob('my-pipeline-build') {
+        definition {
+            cps {
+                script('''
+                    pipeline {
+                        agent any
+                        stages {
+                            stage('Build') {
+                                steps {
+                                    echo 'Building..'
+                                }
+                            }
+                            stage('Test') {
+                                steps {
+                                    echo 'Testing..'
+                                }
+                            }
+                            stage('Deploy to Staging') {
+                                steps {
+                                    echo 'Deploying to staging..'
+                                }
+                            }
+                            stage('Deploy to Production') {
+                                steps {
+                                    echo 'Deploying to production..'
+                                }
+                            }
+                        }
+                    }
+                ''')
+            }
+        }
+    }
+} 
+
 deliveryPipelineView("My Application Pipeline") {
     pipelineInstances(5)
     showAggregatedPipeline()
