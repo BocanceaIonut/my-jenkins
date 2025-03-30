87dfc0f0e3c469fa6959bdfc66e3c3cd20277746
i.bocancea <i.bocancea@klarna.com>
2025-03-30 13:46:00 +0200

    JOBS DSL

---
+++ jobs/pipeline.groovy
@@ -0,0 +1,15 @@
+import service.MyPipeline
+import javaposse.jobdsl.dsl.DslFactory
+
+deliveryPipelineView("My Application Pipeline") {
+    pipelineInstances(5)
+    showAggregatedPipeline()
+    columns(1)
+    updateInterval(60)
+    enableManualTriggers()
+    showAvatars()
+    showChangeLog()
+    pipelines {
+        component("My Application", new MyPipeline(this as DslFactory).build.getName())
+    }
+} 
